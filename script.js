fe04e44732af071f86eb3b242c76551361a81da4
Anthony Ho <ahomnky11@gmail.com>
2015-03-23 11:41:45 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var randomNumber = Math.floor(Math.random() * 6) +1;
+var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+
+if (parseInt(guess) === randomNumber){
+  document.write('<p> You guessed the numner! </p>');
+  
+} else {
+  
+  document.write('<p> Sorry. The number was ' + randomNumber + '</p>');
+  
+}
